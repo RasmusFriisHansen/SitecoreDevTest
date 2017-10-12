4f3f135fe84d1930c464c8e67c2e427f2a653293
Rasmus Friis Hansen <rfh@alpha-solutions.dk>
2017-10-12 15:46:14 +0200

    Sitecore8, page 81 Added Mypage view with additional txt

---
+++ SitecoreDev.Web/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace SitecoreDev.Web
+{
+  public class MvcApplication : Sitecore.Web.Application
+  {
+    protected void Application_Start()
+    {
+      AreaRegistration.RegisterAllAreas();
+      RouteConfig.RegisterRoutes(RouteTable.Routes);
+    }
+  }
+}
